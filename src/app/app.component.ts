c08490bd3eb9ff0f07a43fd034840c776b8047c8
Chen, Yi-Cyuan <emn178@gmail.com>
2017-06-15 14:44:32 +0800

    added angular2-chartjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  type = 'line';
+  data = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "My First dataset",
+        data: [65, 59, 80, 81, 56, 55, 40]
+      }
+    ]
+  };
+  options = {
+    responsive: true,
+    maintainAspectRatio: false,
+
+    // Container for zoom options
+    zoom: {
+      // Boolean to enable zooming
+      enabled: true,
+
+      // Zooming directions. Remove the appropriate direction to disable 
+      // Eg. 'y' would only allow zooming in the y direction
+      mode: 'xy',
+    }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { ChartModule } from 'angular2-chartjs';
+import 'chartjs-plugin-zoom';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    ChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
