8a40659863573ddd08dfe3627fb19f5eb6287f2c
Yi-Cyuan Chen <emn178@gmail.com>
2018-05-13 10:55:36 +0800

    upgrade angular 5

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { Ng4Page } from './app.po';
+import { AppPage } from './app.po';
 
-describe('ng4 App', () => {
-  let page: Ng4Page;
+describe('angular-chartjs-sample App', () => {
+  let page: AppPage;
 
   beforeEach(() => {
-    page = new Ng4Page();
+    page = new AppPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
   });
 });
